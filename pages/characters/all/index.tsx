eb0606b9d717b4a8db3ed235d5dc3a34f1281409
RyGuy8806 <L1ghtSab3r11!!>
2022-12-07 21:36:16 -0800

    Finished Chacter List (except for stylings).

--- pages/characters/all/index.tsx
+++ pages/characters/all/index.tsx
@@ -6,26 +6,31 @@ type CharacterArray = {
     characters: Character[]
 }
 
+const linkStyles = {display: 'flex', border: 'solid 1px black', width: '30%', padding: '1%'}
+
 const defaultCharacterList: Character[] = [new Character("Example Character", "Human", ["Barbarian"], [1], 0)]
 
 const All = ({characters}: CharacterArray) => {
 
-    if(characters && characters.length === 0) characters = defaultCharacterList;
+    
     
     return (
         <>
             <h2>Character List</h2>
             <ul>
-                {characters && characters.map((character) => (
-                    <li key={character.getID()}>
-                       <Link href={`/characters/${character.getID()}`}>
+                {characters && characters.length < 0 ? characters.map((character) => (
+                    <Link href={`/characters/${character.getID()}`} key={character.getID()}>
+                        <li  style={linkStyles}>
                             <h3>{character.getName()}</h3>
                             <h4>{character.getSpecies()}</h4>
                             <h4>{character.classesAndLevelsToString()}</h4>
-                        </Link> 
-                    </li>
-                    
-                ))}
+                        </li>
+                    </Link>
+                )) : <li key={0} style={linkStyles}><Link href={`/characters/0`}>
+                    <h3>Example Character</h3>
+                    <h4>Human</h4>
+                    <h4>Barbarian 1</h4>
+                </Link></li>}
             </ul>
         </>
     )
